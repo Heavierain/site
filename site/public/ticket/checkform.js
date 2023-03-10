f24ed424b65074070e656923c1517d64c1b86651
kieranp2329 <deadflyguitar@gmail.com>
2023-03-10 09:10:23 +0000

    Delete Confusing Hugo

--- site/public/ticket/checkform.js
+++
@@ -1,8 +0,0 @@
-function question1() {
-var value = document.getElementById("question1").value;
-  if (value ==Cuddles) {
-     window.location.href="Cuddles.html";
-  } else if (value == Kisses) {
-     window.alert("Kisses.html");
-  }
-}
--- site/public/ticket/ticketscript.js
+++
@@ -1,4 +0,0 @@
-function showRandomNumber(){
-document.getElementById("myNumber").innerHTML = rando(11111111, 99999999);
-}
-
